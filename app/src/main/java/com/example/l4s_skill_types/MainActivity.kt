ed705c894b06ccf756b13b1dc35a442ec1ca8e17
numabesachi <numabesachi@gmail.com>
2020-09-15 14:23:44 +0900

    finish

---
+++ app/src/main/java/com/example/l4s_skill_types/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.l4s_skill_types
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+//    val hello :String = "おはよう"
+//    println(hello)
+//    おはよう
+//
+//    val name :string = "ランディ"
+//    println("僕の名前は" + name + "だよ！")
+//    error: unresolved reference: string
+//    val name :string = "ランディ"
+//    ^
+//
+//    val name :String = "ランディ"
+//    println("僕の名前は" + name + "だよ！")
+//    僕の名前はランディだよ！
+//
+//    val name :String = "ランディ"
+//    println("僕の名前は${name}だよ！")
+//    僕の名前はランディだよ！
+//
+//    val year :Int = 2020
+//    println("今年は${year}です")
+//    今年は2020です
+//
+//    var name :String = "ランディ"
+//    val age :Int = 6
+//    name = age.toString()
+//
+//    println(name)
+//    6
+//
+//    val long = age.toLong()
+//    println(long)
+//    6
+//
+//    val boolean :Boolean = true
+//    println("Boolean: ${boolean}")
+//    Boolean: true
+//
+//    val number :Int = 5
+//    val answer :Boolean = (3 < number && number < 7)
+//    if(answer){
+//        println("${number}は３より大きくて、７より小さい")
+//    }else{
+//        println("${number}は３より小さい、または７より大きい")
+//    }
+//    5は３より大きくて、７より小さい
+}
